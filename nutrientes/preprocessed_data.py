86a195e351ac26eb8077da3dc523919c3272aef2
Alejandro <mara80@gmail.com>
2015-08-25 10:11:09 -0500

    ranking perfil added

--- nutrientes/preprocessed_data.py
+++ nutrientes/preprocessed_data.py
@@ -37,7 +37,7 @@ def ranking_global_perfil():
     import csv
 
     features = Recipe.create_generic_features()
-    rnv_type = 2
+    rnv_type = 1
     perfiles = perfiles(rnv_type=rnv_type)
     perfiles_dict = {}
     for genero, edad, unidad_edad in perfiles:
@@ -51,18 +51,34 @@ def ranking_global_perfil():
         if key not in perfiles_dict:
             perfiles_dict[key] = {"edad": edad, "genero": genero, "unidad_edad": unidad_edad, "rnv_type": rnv_type}
         
+    edad_range_meses = ["0-6", "7-12"]
+    edad_range_years = ["1-3", "4-8", "9-13", "14-18", "19-30", "31-50", "51-70", "71-150"]
+    def get_range(edad, edad_range_list):
+        for edad_range in edad_range_list:
+            min_year, max_year = edad_range.split("-")
+            min_year = int(min_year)
+            if max_year == '':            
+                max_year = 200
+            else:
+                max_year = int(max_year)
+            if min_year <= perfil["edad"] <= max_year:
+                return edad_range
+
     with open(PREPROCESSED_DATA_DIR+'order_matrix.csv', 'rb') as csvfile:
         rows = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
         for perfil in perfiles_dict.values():
+            print(perfil)
             foods = []
+            if perfil["unidad_edad"] == u"meses":
+                edad_range = get_range(perfil["edad"], edad_range_meses)
+            else:
+                edad_range = get_range(perfil["edad"], edad_range_years)
             for row in rows:
                 food = Food(row[0])
                 score = food.score(perfil, features=features)
-                foods.append((food.ndb_no, score))
-                print foods, perfil
-                break
-        #foods_order = sorted(foods, key=lambda x:x[1], reverse=True)
-        #print foods_order[:10]
+                foods.append((food.ndb_no, score, edad_range, perfil["genero"], perfil["unidad_edad"].encode("utf8", "replace"), perfil["rnv_type"]))
+            ranking_by_perfil(foods)
+            #break
 
 def ranking_category(group):
     from nutrientes.utils import Rank
@@ -117,6 +133,51 @@ def ranking_by_type(data, type_position):
         cursor.execute(query)
         conn.commit()
 
+
+def ranking_by_perfil(data):
+    conn, cursor = conection()
+    for ndb_no, score, edad_range, genero, unidad_edad, rnv_type in data:
+        query = """SELECT COUNT(*) 
+                    FROM ranking_perfil 
+                    WHERE ndb_no='{ndb_no}'
+                    AND edad_range='{edad_range}'
+                    AND genero='{genero}'
+                    AND unidad_edad='{unidad_edad}'
+                    AND rnv_type={rnv_type}""".format(
+                ndb_no=ndb_no, 
+                edad_range=edad_range, 
+                genero=genero, 
+                unidad_edad=unidad_edad, 
+                rnv_type=rnv_type)
+
+        cursor.execute(query)
+        if cursor.fetchall()[0][0] == 1:
+            query = """UPDATE ranking_perfil 
+                        SET score={score}
+                        WHERE ndb_no='{ndb_no}'
+                        AND edad_range='{edad_range}'
+                    AND genero='{genero}'
+                    AND unidad_edad='{unidad_edad}'
+                    AND rnv_type={rnv_type}""".format(
+                ndb_no=ndb_no,
+                score=score,
+                edad_range=edad_range, 
+                genero=genero, 
+                unidad_edad=unidad_edad, 
+                rnv_type=rnv_type)
+        else:
+            query = """INSERT INTO ranking_perfil (ndb_no, score, edad_range, unidad_edad, genero, rnv_type) VALUES ('{ndb_no}', {score}, '{edad_range}', '{unidad_edad}', '{genero}', '{rnv_type}')""".format(
+            ndb_no=ndb_no, 
+            score=score,
+            edad_range=edad_range, 
+            genero=genero, 
+            unidad_edad=unidad_edad, 
+            rnv_type=rnv_type)
+        cursor.execute(query)
+        conn.commit()
+    conn.close()
+
+
 def calc_avg(force=False):
     from nutrientes.utils import avg_omega, avg_nutrients, OMEGAS
     nutavg_vector = Food.get_matrix(PREPROCESSED_DATA_DIR + "nutavg.p")
@@ -140,41 +201,6 @@ def insert_update_db_ranking():
     data = ranking_global_perfil()
     ranking_by_type(data, "global")
 
-
-#def ranking_detail_by_type(ndb_no, data):
-#    conn, cursor = conection()
-#    i = 0
-#    for nutr_no, position in data.items():
-#        print(i)
-#        i += 1
-#        query = """ SELECT COUNT(*) 
-#                    FROM ranking_food_detail
-#                    WHERE ndb_no='{ndb_no}'
-#                    AND nutr_no='{nutr_no}'""".format(
-#                ndb_no=ndb_no,
-#                nutr_no=nutr_no)
-#        cursor.execute(query)
-#        if cursor.fetchall()[0][0] == 1:
-#            query = """UPDATE ranking_food_detail 
-#                        SET position={position}
-#                        WHERE ndb_no='{ndb_no}'
-#                        AND nutr_no='{nutr_no}'""".format(
-#                ndb_no=ndb_no,
-#                position=position,
-#                nutr_no=nutr_no)
-#        else:
-#            query = """INSERT INTO ranking_food_detail 
-#                        VALUES ('{ndb_no}', '{nutr_no}', {position})""".format(
-#            ndb_no=ndb_no, 
-#            position=position,
-#            nutr_no=nutr_no)
-#        cursor.execute(query)
-#        conn.commit()
-
-#def calc_ranking_detail(rank):
-#    for ndb_no in rank.ranks:
-#        data = rank.get_values_food(ndb_no)
-#        ranking_detail_by_type(ndb_no, data)
 
 def recalc_preprocessed_data():
     print "Generate AVG"
