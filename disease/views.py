5c422f50ef6ad613849a3f523e7c58a8c4ebfc35
Alejandro <mara80@gmail.com>
2015-09-28 17:44:46 -0500

    added diabetes page

--- disease/views.py
+++ disease/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from disease import models as models_disease
 
-# Create your views here.
 def diabetes(request):
-    return render(request, "diabetes.html", {})
+    from collections import OrderedDict
+    mortality_years = models_disease.MortalityYears.objects.all().order_by("-year", "-amount")
+    mortality_list = OrderedDict()
+    years = list(range(2000, 2014))
+    years.reverse()
+    for mortality in mortality_years:
+        mortality_list.setdefault(mortality.causa_mortality.description, [])
+        mortality_list[mortality.causa_mortality.description].append(mortality.amount)
+    return render(request, "diabetes.html", {"years": years, "mortality_list": mortality_list})
