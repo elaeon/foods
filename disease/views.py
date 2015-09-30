e250008e42be98385e311f7b90a89e041a294182
Alejandro <mara80@gmail.com>
2015-09-30 12:05:44 -0500

    added grown rate

--- disease/views.py
+++ disease/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from disease import models as models_disease
 
@@ -7,7 +8,18 @@ def diabetes(request):
     mortality_list = OrderedDict()
     years = list(range(2000, 2014))
     years.reverse()
+    disease_relate = set([
+        u"Enfermedades isquémicas del corazón",
+        "Enfermedad cerebrovascular",
+        "Enfermedades hipertensivas",
+        "Nefritis y nefrosis"])
     for mortality in mortality_years:
         mortality_list.setdefault(mortality.causa_mortality.description, [])
         mortality_list[mortality.causa_mortality.description].append(mortality.amount)
-    return render(request, "diabetes.html", {"years": years, "mortality_list": mortality_list})
+    for k, amount in mortality_list.items():
+        growth_rate = round((((amount[0]/float(amount[-1]))**(1/13.)) - 1) * 100, 2)
+        mortality_list[k].append(growth_rate)
+    return render(request, "diabetes.html", {
+        "years": years, 
+        "mortality_list": mortality_list,
+        "disease_relate": disease_relate})
