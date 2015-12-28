89ff282103c0d2fe530643719437dee5988d45b7
elaeon <mara80@gmail.com>
2015-12-28 15:54:20 -0600

    more data in disease template

--- disease/urls.py
+++ disease/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from disease import views as disease_views
 
 urlpatterns = [
-    url(r'^$', disease_views.index, name="index"),
+    url(r'^$', disease_views.index, name="disease_index"),
     url(r'^diabetes/$', disease_views.diabetes, name="diabetes"),
     url(r'^cancer/$', disease_views.cancer, name="cancer"),
 ]
