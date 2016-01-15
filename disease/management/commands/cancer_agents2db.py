763b96defcd761b23c483e66763e1fd155977907
Alejandro <mara80@gmail.com>
2016-01-15 16:04:47 -0600

    added command

---
+++ disease/management/commands/cancer_agents2db.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from optparse import make_option
+from django.conf import settings
+
+from disease import models as m_disease
+
+#import glob
+#import os
+#import hashlib
+import csv
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--file',
+            action='store',
+            dest='file',
+            help='save data from file to db'),
+        )
+
+    def handle(self, *args, **options):
+        with open('{}/data/causas_cancer.csv'.format(settings.BASE_DIR), 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                cancer, _ = m_disease.Cancer.objects.get_or_create(name=row[0])
+                agent, _ = m_disease.CancerAgent.objects.get_or_create(name=row[1].strip(), nivel=1)
+                m_disease.CancerAgentRelation.objects.get_or_create(cancer=cancer, agent=agent)
+                print(row)
